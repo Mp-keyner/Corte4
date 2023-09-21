1b8a58327608fe9f1dd28ea48d485b0da2efa1f9
Mp-keyner <keyneroswaldo0@gmail.com>
2023-09-21 08:50:10 -0500

    21 de septiembre

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
-import React from 'react';
-import './App.css'
-import List from './components/List';
+import React from "react";
+import "./App.css";
+import List from "./components/List";
+import Slo from "./components/Slo";
 
 const App = () => {
   return (
-    <div className='container'>
-        <List/>
+    <div className="container">
+      <Slo />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
